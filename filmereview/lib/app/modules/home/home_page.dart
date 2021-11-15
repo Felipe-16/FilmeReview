45d1da5563b2a65e63a837471735c801ac91f840
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-11-15 16:03:46 -0300

    Design da page

--- filmereview/lib/app/modules/home/home_page.dart
+++ filmereview/lib/app/modules/home/home_page.dart
@@ -1,5 +1,9 @@
+import 'package:filmereview/app/modules/home/home_controller.dart';
+import 'package:filmereview/app/modules/home/widgets/painel_widget.dart';
 import 'package:filmereview/shared/components/app_bar/appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -8,14 +12,49 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(
-        tituloLogo: 'Home',
-        icon: Icons.local_movies,
-      ),
-    );
+        appBar: AppBarWidget(
+          titulo: 'Home',
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Filmes',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Observer(builder: (_) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: controller.listaFilmes.length,
+                      itemBuilder: (context, index) {
+                        return PainelWidgetCard(
+                          titulo: controller.listaFilmes[index].titulo,
+                          imagem: controller.listaFilmes[index].imagem,
+                          onTap: () {
+                            Modular.to.pushNamed('/home/filme-especifico',
+                                arguments: controller.listaFilmes[index]);
+                          },
+                        );
+                      }),
+                );
+              }),
+            ],
+          ),
+        ));
   }
 }
