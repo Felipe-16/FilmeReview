bc40ab70b4f57b929f1486daa6301210b6a8ae18
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-11-15 19:49:49 -0300

    Widget painel da tela de home

--- filmereview/lib/app/modules/home/widgets/painel_widget.dart
+++ filmereview/lib/app/modules/home/widgets/painel_widget.dart
@@ -18,40 +18,28 @@ class PainelWidgetCard extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: GestureDetector(
         onTap: onTap,
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                color: Colors.blueGrey,
-                width: 1,
-              )),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * 0.13,
-          child: Row(
-            children: [
-              Expanded(
-                flex: 4,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(
-                      '$titulo',
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 4),
-                      child: Text(
-                        '$imagem!',
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  ],
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '$titulo',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  
                 ),
               ),
-            ],
-          ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(imagem!), fit: BoxFit.fill),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
