25ed76c0b562be0b5bdd1a74014e2aa52990fb4f
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-11-16 00:02:10 -0300

    Criação do controller para login

---
+++ filmereview/lib/app/modules/login/login_controller.dart
@@ -0,0 +1,58 @@
+import 'package:mobx/mobx.dart';
+
+part 'login_controller.g.dart';
+
+class LoginController = _LoginController with _$LoginController;
+
+abstract class _LoginController with Store {
+  // final AuthController authController;
+
+  // _LoginController(this.authController);
+
+  @observable
+  bool passwordVisibility = true;
+
+  @observable
+  String erros = '';
+
+  @observable
+  bool isChecked = false;
+
+  @observable
+  String email = '';
+
+  @observable
+  String password = '';
+
+  @action
+  void setIsChecked(bool? value) {
+    isChecked = value ?? false;
+  }
+
+  @action
+  void setEmail(String value) {
+    email = value;
+  }
+
+  @action
+  void setPassword(String value) {
+    password = value;
+  }
+
+  // @action
+  // Future<void> login() async {
+  //   try {
+  //     await authController.loginWithEmail(email, password, isChecked);
+  //     if (authController.isLogged) {
+  //       Modular.to.navigate('/home');
+  //     }
+  //   } on Failure catch (e) {
+  //     erros = e.message;
+  //   }
+  // }
+
+  // @action
+  // void changePasswordVisibility() {
+  //   passwordVisibility = !passwordVisibility;
+  // }
+}
