302f9bb8369ee43ba20a25c62666226b0110ebd8
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-11-16 00:01:45 -0300

    Estruturação MVC para a tela de Login

---
+++ filmereview/lib/app/modules/login/login_module.dart
@@ -0,0 +1,19 @@
+import 'package:filmereview/app/modules/cadastro/cadastro_module.dart';
+import 'package:filmereview/app/modules/home/home_module.dart';
+import 'package:filmereview/app/modules/login/login_controller.dart';
+import 'package:filmereview/app/modules/login/login_page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class LoginModule extends Module {
+  @override
+  final List<Bind> binds = [
+    Bind.lazySingleton((i) => LoginController()),
+  ];
+
+  @override
+  final List<ModularRoute> routes = [
+    ChildRoute(Modular.initialRoute, child: (_, args) => LoginPage()),
+    ModuleRoute('/home', module:HomeModule()),
+    ModuleRoute('/cadastro', module: CadastroModule()),
+  ];
+}
