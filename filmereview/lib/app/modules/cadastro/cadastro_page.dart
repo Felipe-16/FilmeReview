71a0b452fa06b3494dc1e155e318b0433b0dbf08
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-11-16 00:52:30 -0300

    Criando page view do Cadastro

---
+++ filmereview/lib/app/modules/cadastro/cadastro_page.dart
@@ -0,0 +1,85 @@
+import 'package:filmereview/shared/components/app_bar/appbar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class CadastroPage extends StatefulWidget {
+  CadastroPage({Key? key}) : super(key: key);
+
+  @override
+  _CadastroPageState createState() => _CadastroPageState();
+}
+
+class _CadastroPageState extends State<CadastroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBarWidget(
+          titulo: 'Cadastro',
+          icon: Icons.account_box,
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'PocPloc Movies',
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40),
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Criar nova conta?',
+                      style: TextStyle(fontSize: 20),
+                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: null,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Nome de usuario',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: null,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: ' Email',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  child: TextField(
+                    obscureText: true,
+                    controller: null,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Senha',
+                    ),
+                  ),
+                ),
+                Container(
+                    height: 50,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Modular.to.pushNamed('/home');
+                      },
+                      style: ElevatedButton.styleFrom(primary: Colors.red[700]),
+                      child: Text('Criar nova conta'),
+                    )),
+              ],
+            )));
+  }
+}
