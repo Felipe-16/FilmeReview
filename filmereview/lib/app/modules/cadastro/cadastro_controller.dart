a8b561661a314b0b1b7427e5983ffe1bb6017de3
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-11-16 00:03:48 -0300

    Criando controller para cadastro

---
+++ filmereview/lib/app/modules/cadastro/cadastro_controller.dart
@@ -0,0 +1,20 @@
+import 'package:mobx/mobx.dart';
+
+part 'cadastro_controller.g.dart';
+
+class CadastroController = _CadastroControllerBase with _$CadastroController;
+
+abstract class _CadastroControllerBase with Store {
+
+  // _HomeControllerBase({required this.repository}) {
+  //   getFilmes();
+  // }
+
+  // @observable
+  // List<HomeModel> listaFilmes = List.empty();
+
+  // @action
+  // Future<void> getFilmes() async {
+  //   listaFilmes = await repository.getFilmes();
+  // }
+}
